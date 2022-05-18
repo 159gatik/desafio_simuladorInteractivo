9aa782aac847849f10c4bac8c283a3e428182f53
159gatik <rojasgaston95@gmail.com>
2022-05-18 14:14:56 -0300

    se agrega la opcion de transferir saldo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,78 @@
-//Poder consultar y extraer saldo de un cajero
+//Poder consultar, extraer y transferir saldo de un cajero
 
 
-let saldoTotal = 30000;
+class Cliente {
+  constructor(id, nombre, apellido, saldo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.saldo = saldo;
+  }
+  mostrarCliente() {
+    alert(`Bienvenido/a ${this.nombre} ${this.apellido}`)
+  }
+
+  getSaldoFormateado() {
+    return "$ " + Intl.NumberFormat('en-IN', {
+      maximumSignificantDigits: 3
+    }).format(this.saldo);
+  }
+}
+
+const nombreIngresado = prompt("Ingresar su nombre");
+const apellidoIngresado = prompt("Ingresar apellido");
+const saldoPorDefecto = 30000;
+const cliente1 = new Cliente(1, nombreIngresado, apellidoIngresado, saldoPorDefecto);
+const cliente2 = new Cliente(2, nombreIngresado, apellidoIngresado, saldoPorDefecto);
+cliente1.mostrarCliente();
 let seguir = true;
 
+const transferir = () => {
+
+  let opcionIncorrectaCbu = true;
+  let cbuDestinatario;
+
+  while (opcionIncorrectaCbu) {
+
+    cbuDestinatario = Number(prompt("Ingresar cbu"));
+
+    if (!isNaN(cbuDestinatario) && cbuDestinatario.toString().length === 18) {
+      opcionIncorrectaCbu = false
+    } else {
+      alert(`Ingrese un cbu valido (Tiene que ser 18 números)`)
+
+    }
+
+  }
+
+  let opcionIncorrecta = true;
+  let saldoATransferir;
+
+  while (opcionIncorrecta) {
+
+    saldoATransferir = Number(prompt("Ingresar saldo"));
+
+    if (saldoATransferir > cliente1.saldo) {
+      alert(`El monto que desea transferir supera el saldo de su cuenta, el saldo maximo a transferir es de: ${cliente1.getSaldoFormateado()}`)
+    } else {
+      opcionIncorrecta = false
+    }
+  }
+
+  cliente1.saldo -= saldoATransferir
+
+  alert(
+    `Usted transfirió $ ${saldoATransferir} al CBU ${cbuDestinatario} . Su saldo restante es ${cliente1.getSaldoFormateado()}`
+  );
+
+}
+
 while (seguir) {
-  let opcion = prompt(`Seleccione la siguiente operación: 
-                1. CONSULTAR SALDO
-                2. EXTRAER DINERO
-                3. SALIR`);
+  let opcion = prompt(`Hola ${cliente1.nombre}, seleccione la siguiente operación:
+                  1. CONSULTAR SALDO
+                  2. EXTRAER DINERO
+                  3. TRANSFERIR DINERO
+                  4. SALIR`);
 
   switch (opcion) {
     case "1":
@@ -18,6 +82,9 @@ while (seguir) {
       extraccion();
       break;
     case "3":
+      transferir();
+      break;
+    case "4":
       salir();
       seguir = false;
       break;
@@ -28,11 +95,12 @@ while (seguir) {
 }
 
 function consultarSaldo() {
-  alert("Su saldo actual es $" + saldoTotal);
+
+  alert(`Su saldo es: ` + cliente1.getSaldoFormateado());
 }
 
 function salir() {
-  alert("¡NO SE OLVIDE DE RETIRAR LA TARJETA! GRACIAS.");
+  alert("¡NO SE OLVIDE DE RETIRAR LA TARJETA! GRACIAS :) .");
 }
 
 function extraccion() {
@@ -40,10 +108,10 @@ function extraccion() {
   let dineroIngresado;
   while (opcionCorrecta === false) {
     dineroIngresado = Number(
-      prompt(`¿Cuanto quiere retirar? Solo puede ingresar:
-                                        $2500 
-                                        $4000 
-                                        $10000`)
+      prompt(`¿Cuánto querés retirar ${cliente1.nombre} ? Solo puede ingresar:
+                                          $2500 
+                                          $4000 
+                                          $10000`)
     );
     console.log(dineroIngresado);
     if (
@@ -52,14 +120,12 @@ function extraccion() {
       dineroIngresado === 10000
     ) {
       opcionCorrecta = true;
-      saldoTotal -= dineroIngresado;
-      console.log(saldoTotal);
+      cliente1.saldo -= dineroIngresado;
     } else {
-      alert("El monto es invalido, pruebe de nuevo");
+      alert("El monto es invalido :( , pruebe de nuevo");
     }
   }
-
   alert(
-    "Usted extrajo $" + dineroIngresado + ". Su saldo restante es $" + saldoTotal
+    "Usted extrajo $" + dineroIngresado + ". Su saldo restante es " + cliente1.getSaldoFormateado()
   );
 }
